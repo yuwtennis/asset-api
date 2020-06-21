e59c6cd6d56dadc458e07f727faf78558b9e6e87
ywatanabe <yu.w.tennis@gmail.com>
2020-06-21 18:58:48 +0900

    Updated scraping script for Nomura securities.
    Updated scraping script for MUFG.

---
+++ sandbox/parse-mufg.py
@@ -0,0 +1,24 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get('https://entry11.bk.mufg.jp/ibg/dfw/APLIN/loginib/login?_TRANID=AA000_001')
+
+driver.find_element_by_name('KEIYAKU_NO').send_keys(os.getenv('MUFG_ACCOUNT_NUM'))
+driver.find_element_by_name('PASSWORD').send_keys(os.getenv('MUFG_SECRET'))
+
+driver.find_element_by_class_name('gonext').click()
+driver.find_element_by_xpath('//div[@class="card-body"]/section[5]/a[3]').click()
+driver.find_element_by_xpath('//div[@id="contents"]/div/div[3]/div/div/div/div/ul/li[1]').click()
+driver.find_element_by_xpath('//input[@value="選択"]').click()
+
+term_deposit = driver.find_element_by_xpath('//div[@class="serviceContents"]/div[3]/table/tbody/tr/td[5]').text
+
+print(f'{term_deposit}')
+
+driver.find_element_by_link_text('ログアウト').click()
+driver.quit()
--- sandbox/parse-nomura-securities.py
+++ sandbox/parse-nomura-securities.py
@@ -8,21 +8,20 @@ driver = webdriver.Chrome(options=chrome_options)
 
 driver.get('https://hometrade.nomura.co.jp/web/mfIndexWebAction.do')
 
-btnCode = driver.find_element_by_name('btnCd')
-kuzNo   = driver.find_element_by_name('kuzNo')
-gnziLoginPswd = driver.find_element_by_name('gnziLoginPswd')
+btnCode       = driver.find_element_by_name('btnCd').send_keys(os.getenv('NOMURA_SEC_BRANCH_CODE'))
+kuzNo         = driver.find_element_by_name('kuzNo').send_keys(os.getenv('NOMURA_SEC_ACCOUNT_NUM'))
+gnziLoginPswd = driver.find_element_by_name('gnziLoginPswd').send_keys(os.getenv('NOMURA_SEC_SECRET'))
 
-btnCode.send_keys(os.getenv('BTNCODE'))
-kuzNo.send_keys(os.getenv('KUZNO'))
-gnziLoginPswd.send_keys('GNZILOGINPSWD')
 driver.find_element_by_name('buttonLogin').click()
 
-# TODO
-# Need to parse table
-table = find_element_by_class_name('asset-summary-data')
+dd          = driver.find_element_by_class_name('ct-top-asset-summary-row')
+table       = dd.find_element_by_class_name('asset-summary-data')
+total_asset = table.find_element_by_xpath('//tbody/tr[1]/td').text
+profit      = table.find_element_by_xpath('//tbody/tr[2]/td').text
 
-# TODO
-# Need to click logout
+mrf_remain  = dd.find_element_by_xpath('//div/div/p[2]').text
 
-# TODO
-# Need to quit browser
+print(f'{total_asset},{profit},{mrf_remain}')
+
+driver.find_element_by_link_text('ログアウト').click()
+driver.quit()
